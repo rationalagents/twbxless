fb1e027a1b3eeff9fccec756a4ad9ae52c43b38c
Wade Dorrell <wade@dorrells.org>
2020-05-16 19:06:44 -0600

    Match comment to implementation

--- src/main/java/com/rationalagents/twbxless/HyperUtils.java
+++ src/main/java/com/rationalagents/twbxless/HyperUtils.java
@@ -9,12 +9,7 @@ public class HyperUtils {
 	 * Gets next value from the Result, similar to {@link Result#getString(int)} for specified column in result schema,
 	 * as a String appropriate for CSV output.
 	 *
-	 * For types I didn't expect to handle or know how to handle, the returned value will start with "?" then will
-	 * indicate the unknown type.
-	 *
-	 * Unfortunately Tableau didn't seem to have a version of this, or I didn't find it.
-	 *
-	 * Might consider trying {@link Result#getObject(int)}.
+	 * For types that didn't seem appropriate to convert, the returned value will be "TYPE?"
 	 */
 	static String getString(ResultSchema.Column column, Result result, ResultSchema schema) {
 		int position = schema.getColumnPositionByName(column.getName()).orElseThrow();
